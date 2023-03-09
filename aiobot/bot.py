5cdc83bcb2d44ad1ba607c0988c1c1f0673b9070
hush-tukul <hush-tukul@protonmail.com>
2023-03-09 20:16:04 +0100

    add: bot.py

---
+++ aiobot/bot.py
@@ -0,0 +1,20 @@
+import asyncio
+from aiogram import Bot, Dispatcher
+from run import token
+
+
+
+
+# Запуск бота
+async def main():
+    bot = Bot(token=token)
+    dp = Dispatcher()
+
+    # Запускаем бота и пропускаем все накопленные входящие
+    # Да, этот метод можно вызвать даже если у вас поллинг
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
